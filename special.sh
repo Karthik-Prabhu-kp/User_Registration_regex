a67856c99cff1b111d962357fbb6629458a940e0
Karthik-Prabhu-kp <kpwat66@gmail.com>
2020-09-05 12:57:44 +0530

    uc8

---
+++ special.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+read -p "enter password" password
+
+regex="^[a-z0-9@#!/-](?=.*?[#?!@%/-])(?=.*?[0-9])(?=.*[[:upper:]]){8,}$"
+
+if [[ $password =~ $regex ]]
+then
+        echo "password is valid"
+else
+        echo "password not valid"
+f
